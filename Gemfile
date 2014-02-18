b171ae5e685655b1e91eb4b912be3751b52368b1
peterskd <kyle.peters@taxware.com>
2014-02-18 10:35:27 -0500

    added ey.yml

--- Gemfile
+++ Gemfile
@@ -5,7 +5,13 @@ gem 'rails', '4.0.2'
 
 # Use sqlite3 as the database for Active Record
 group :development do
+  gem 'rspec-rails'
   gem 'sqlite3'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
 end
 
 #Adds the prostgres gem
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 FirstApp::Application.config.secret_key_base = '0156667b30486da7f45192c8e49f100b86da6e92e4765f04e7757fdc60861aba9919eefa213b0e41ae6ff57d364ea548555ee0a3b597119fd25ff99a2b6a2d36'
